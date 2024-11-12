19bb8aea2be156e0690d91e4fcbaa1f5c278fec9
MbilawalRaza <155060676+MbilawalRaza@users.noreply.github.com>
2024-11-12 08:48:11 -0800

    card dashboard dynamic
    dynamic card dashboard by js

---
+++ app.js
@@ -0,0 +1,223 @@
+
+
+var postObjs = [
+    {
+         img: "https://img.drz.lazcdn.com/static/pk/p/53719d588e3e5419b05e353c1e7f812d.jpg_720x720q80.jpg_.webp",
+         desc: 'dummmy',
+         price: 500,
+         rating: 5
+       }
+       ,
+       {
+        img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+        desc: 'testing',
+        price: 800,
+        rating: 3
+      },
+      {
+        img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+        desc: 'testing',
+        price: 800,
+        rating: 3
+      },
+      {
+        img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+        desc: 'testing',
+        price: 800,
+        rating: 3
+      },
+      {
+        img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+        desc: 'testing',
+        price: 800,
+        rating: 3
+      },
+      {
+        img: "https://img.drz.lazcdn.com/static/pk/p/53719d588e3e5419b05e353c1e7f812d.jpg_720x720q80.jpg_.webp",
+        desc: 'dummmy',
+        price: 500,
+        rating: 5
+      }
+      ,
+      {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+        img: "https://img.drz.lazcdn.com/static/pk/p/53719d588e3e5419b05e353c1e7f812d.jpg_720x720q80.jpg_.webp",
+        desc: 'dummmy',
+        price: 500,
+        rating: 5
+      }
+      ,
+      {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+        img: "https://img.drz.lazcdn.com/static/pk/p/53719d588e3e5419b05e353c1e7f812d.jpg_720x720q80.jpg_.webp",
+        desc: 'dummmy',
+        price: 500,
+        rating: 5
+      }
+      ,
+      {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+        img: "https://img.drz.lazcdn.com/static/pk/p/53719d588e3e5419b05e353c1e7f812d.jpg_720x720q80.jpg_.webp",
+        desc: 'dummmy',
+        price: 500,
+        rating: 5
+      }
+      ,
+      {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+     {
+       img: 'https://img.drz.lazcdn.com/static/pk/p/acd6b6946eaa3fa6267296014dc3503a.jpg_720x720q80.jpg_.webp',
+       desc: 'testing',
+       price: 800,
+       rating: 3
+     },
+ ]
+
+ for(var i=0; i<postObjs.length; i++){
+    console.log(postObjs[i].img)
+ }
+
+ var parent = document.getElementById('parent')
+
+ 
+ function showData(){
+    for(var i=0; i<postObjs.length; i++){
+        var div = document.createElement("div")
+        div.style.border = "1px solid #ddd";
+        div.style.padding = "10px";
+        div.style.margin = "10px";
+        div.style.width = "200px";
+
+    
+
+        var divImg = document.createElement('img')
+        divImg.setAttribute('src', postObjs[i].img)
+        divImg.setAttribute('class', 'height')
+        divImg.style.width = "100%"; 
+        div.appendChild(divImg)
+
+
+       
+       var pchilddiv =  document.createElement("div")
+       
+
+
+       var price= document.createElement("div")
+var pricetxt = document.createTextNode("Price: $" + postObjs[i].price)
+pchilddiv.appendChild(price)
+ pchilddiv.appendChild(pricetxt)
+
+
+       var decptriondiv =document.createElement("div")    
+var decptriontext =document.createTextNode("Description: " + postObjs[i].desc)
+pchilddiv.appendChild(decptriondiv);
+pchilddiv.appendChild(decptriontext)
+ 
+
+var ratingdiv =   document.createElement("div")
+pchilddiv.appendChild(ratingdiv);
+var ratingtxt =document.createTextNode("Rating: " + postObjs[i].rating)
+pchilddiv.appendChild(ratingtxt)
+
+
+
+      pchilddiv.style.display = "flex";
+      pchilddiv.style.flexDirection = "column";
+      pchilddiv.style.gap = "5px";
+      div.appendChild(pchilddiv);
+
+      parent.appendChild(div);
+     
+     }
+
+
+ }
+
+ showData()
+
+
